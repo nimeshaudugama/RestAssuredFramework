73159e8d90e98f3cc0e3fb28e27f3baf1a583d1d
Nimesha Udugama <130186697+nimeshaudugama@users.noreply.github.com>
2024-02-16 23:40:18 -0500

    Update DeletePost.java

--- src/test/java/com/RestAssuredTesting/DeletePost.java
+++ src/test/java/com/RestAssuredTesting/DeletePost.java
@@ -24,7 +24,7 @@ public class DeletePost {
 	public void getData() throws IOException {
 	
 
-	FileInputStream fis = new FileInputStream("/Users/nimesha/eclipse-workspace/RestAssuredTesting/src/main/java/properties/environment.properties");
+	FileInputStream fis = new FileInputStream((System.getProperty("user.dir")+"/src/main/java/properties/environment.properties"));
 	pro.load(fis);
 	pro.get("HOST");
 	
